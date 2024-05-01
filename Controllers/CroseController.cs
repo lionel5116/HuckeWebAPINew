d8471d3904059556144158b4cc349422821909ab
Jones <lioneljones5116@gmail.com>
2024-05-01 12:45:39 -0500

    first commit

--- Controllers/CroseController.cs
+++ Controllers/CroseController.cs
@@ -9,6 +9,8 @@ using System.Configuration;
 using System.Data;
 using System.Data.Common;
 using System.Data.SqlClient;
+using System.IO;
+using System.Security.Cryptography;
 
 using System.Web.Script.Serialization;
 
@@ -19,6 +21,8 @@ namespace HuckeWEBAPI.Controllers
     {
 
         public string s_ConnectionStringCROSE = ConfigurationManager.AppSettings["CONN_STRING_CROSE"].ToString();
+        public string s_ConnectionStringCROSE_PROD_DB = ConfigurationManager.AppSettings["CONN_STRING_CROSE_PROD_DB"].ToString();
+        //
         public string s_DBaseProvider = ConfigurationManager.AppSettings["currProvider"].ToString();
         public string s_Environment = ConfigurationManager.AppSettings["Environment"].ToString();
 
@@ -45,6 +49,13 @@ namespace HuckeWEBAPI.Controllers
             List<VendorCROSE> lstCROSEData = new List<VendorCROSE>();
             var connectionString = s_ConnectionStringCROSE;
 
+            //string dbPassword = DecryptString(s_ConnectionStringCROSE, "CroseApp2024");
+
+          
+            var ConnectionStringPrivate = "server=DWQADB01;uid=app_daep;pwd=";
+            //ConnectionStringPrivate += dbPassword;
+            ConnectionStringPrivate += ";database=EDB";
+
             string SQLCommandText = $"SELECT (VendorNumber + CONVERT(CHAR,[CheckDate]))as random,VendorNumber,Name,CheckNumber, CheckDate,Amount FROM [EDB].[EXT].[VendorCROSE] ";
             //string SQLCommandText = $"SELECT (VendorNumber + CONVERT(CHAR,[CheckDate]))as random,VendorNumber,Name,CheckNumber, CheckDate,Amount FROM VendorData";
 
@@ -70,6 +81,7 @@ namespace HuckeWEBAPI.Controllers
             SQLCommandText += " ORDER BY LTRIM(RTRIM(Name)),CheckDate";
 
             using (SqlConnection CONN = new SqlConnection(connectionString))
+            //using (SqlConnection CONN = new SqlConnection(ConnectionStringPrivate))
             {
                 using (SqlCommand cmd = new SqlCommand(SQLCommandText, CONN))
                 {
@@ -114,9 +126,17 @@ namespace HuckeWEBAPI.Controllers
             List<VendorCROSE> lstCROSEData = new List<VendorCROSE>();
             var connectionString = s_ConnectionStringCROSE;
 
+            //string dbPassword = DecryptString(s_ConnectionStringCROSE, "CroseApp2024");
+
+
+            var ConnectionStringPrivate = "server=DWQADB01;uid=app_daep;pwd=";
+            //ConnectionStringPrivate += dbPassword;
+            ConnectionStringPrivate += ";database=EDB";
+
             string SQLCommandText = $"SELECT distinct(VendorNumber), Name FROM [EDB].[EXT].[VendorCROSE] ";
             SQLCommandText += " ORDER BY Name";
 
+            //using (SqlConnection CONN = new SqlConnection(ConnectionStringPrivate))
             using (SqlConnection CONN = new SqlConnection(connectionString))
             {
                 using (SqlCommand cmd = new SqlCommand(SQLCommandText, CONN))
@@ -143,8 +163,95 @@ namespace HuckeWEBAPI.Controllers
                 }
             }
 
+            return lstCROSEData;
+        }
+
+        [Route("api/crose/getCroseVendorList_PROD_DB/")]
+        [HttpGet]
+        public List<VendorCROSE> getCroseVendorList_PROD_DB()
+        {
+
+            VendorCROSE oVendorCROSE;
+            List<VendorCROSE> lstCROSEData = new List<VendorCROSE>();
+            var connectionString = s_ConnectionStringCROSE_PROD_DB;
+
+            string SQLCommandText = $"SELECT distinct(VendorNumber), Name FROM [EDB].[EXT].[VendorCROSE] ";
+            SQLCommandText += " ORDER BY Name";
+
+         
+            using (SqlConnection CONN = new SqlConnection(s_ConnectionStringCROSE_PROD_DB))
+            {
+                using (SqlCommand cmd = new SqlCommand(SQLCommandText, CONN))
+                {
+                    cmd.CommandType = CommandType.Text;
+                    SqlDataAdapter da = new SqlDataAdapter();
+                    da.SelectCommand = cmd;
+                    DataSet ds = new DataSet();
+                    da.Fill(ds);
+                    int i = 0;
+                    if (ds.Tables[0].Rows.Count > 0) { } else { return null; };
+
+                    foreach (DataRow row in ds.Tables[0].Rows)
+                    {
+                        oVendorCROSE = new VendorCROSE();
+                        oVendorCROSE.random = i.ToString();
+                        oVendorCROSE.VendorNumber = row["VendorNumber"].ToString();
+                        oVendorCROSE.Name = row["Name"].ToString();
+                        lstCROSEData.Add(oVendorCROSE);
+                        oVendorCROSE = null;
+                        i++;
+                    }
+
+                }
+            }
 
             return lstCROSEData;
         }
+
+        private string EncryptString(string plaintext, string password)
+        {
+            // Convert the plaintext string to a byte array
+            byte[] plaintextBytes = System.Text.Encoding.UTF8.GetBytes(plaintext);
+
+            // Derive a new password using the PBKDF2 algorithm and a random salt
+            Rfc2898DeriveBytes passwordBytes = new Rfc2898DeriveBytes(password, 20);
+
+            // Use the password to encrypt the plaintext
+            Aes encryptor = Aes.Create();
+            encryptor.Key = passwordBytes.GetBytes(32);
+            encryptor.IV = passwordBytes.GetBytes(16);
+            using (MemoryStream ms = new MemoryStream())
+            {
+                using (CryptoStream cs = new CryptoStream(ms, encryptor.CreateEncryptor(), CryptoStreamMode.Write))
+                {
+                    cs.Write(plaintextBytes, 0, plaintextBytes.Length);
+                }
+                return Convert.ToBase64String(ms.ToArray());
+            }
+        }
+
+        private string DecryptString(string encrypted, string password)
+        {
+            // Convert the encrypted string to a byte array
+            byte[] encryptedBytes = Convert.FromBase64String(encrypted);
+
+            // Derive the password using the PBKDF2 algorithm
+            Rfc2898DeriveBytes passwordBytes = new Rfc2898DeriveBytes(password, 20);
+
+            // Use the password to decrypt the encrypted string
+            Aes encryptor = Aes.Create();
+            encryptor.Key = passwordBytes.GetBytes(32);
+            encryptor.IV = passwordBytes.GetBytes(16);
+            using (MemoryStream ms = new MemoryStream())
+            {
+                using (CryptoStream cs = new CryptoStream(ms, encryptor.CreateDecryptor(), CryptoStreamMode.Write))
+                {
+                    cs.Write(encryptedBytes, 0, encryptedBytes.Length);
+                }
+                return System.Text.Encoding.UTF8.GetString(ms.ToArray());
+            }
+        }
+
+
     }
 }
