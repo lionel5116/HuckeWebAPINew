3ff34c4e4d424e4ba1dec04bc69f60e1e3414734
Jones <lioneljones5116@gmail.com>
2024-06-04 13:03:28 -0500

    more endpoints

--- Controllers/CroseController.cs
+++ Controllers/CroseController.cs
@@ -14,6 +14,7 @@ using System.Security.Cryptography;
 
 using System.Text.Json.Serialization;
 using System.Text.Json;
+using Newtonsoft.Json;
 
 using System.Web.Script.Serialization;
 
@@ -50,7 +51,58 @@ namespace HuckeWEBAPI.Controllers
 
         }
 
+     
 
+        [Route("api/crose/getAllCroseVendorData/")]
+        [HttpGet]
+        public List<VendorCROSE> getAllCroseVendorData()
+        {
+            VendorCROSE oVendorCROSE;
+            List<VendorCROSE> lstCROSEData = new List<VendorCROSE>();
+            var connectionString = s_ConnectionStringCROSE;
+          
+            var ConnectionStringPrivate = "server=DWQADB01;uid=app_daep;pwd=";
+            ConnectionStringPrivate += ";database=EDB";
+
+            string SQLCommandText = $"SELECT (VendorNumber + CONVERT(CHAR,[CheckDate]))as random,VendorNumber,Name,CheckNumber, CheckDate,Amount FROM [EDB].[EXT].[VendorCROSE] ";
+          
+
+            SQLCommandText += " ORDER BY LTRIM(RTRIM(Name)),CheckDate";
+
+            using (SqlConnection CONN = new SqlConnection(connectionString))
+            {
+                using (SqlCommand cmd = new SqlCommand(SQLCommandText, CONN))
+                {
+                    cmd.CommandType = CommandType.Text;
+                    SqlDataAdapter da = new SqlDataAdapter();
+                    da.SelectCommand = cmd;
+                    DataSet ds = new DataSet();
+                    da.Fill(ds);
+
+                    if (ds.Tables[0].Rows.Count > 0) { } else { return null; };
+                    int i = 0;
+                    foreach (DataRow row in ds.Tables[0].Rows)
+                    {
+                        oVendorCROSE = new VendorCROSE();
+                        oVendorCROSE.random = i.ToString();
+                        oVendorCROSE.VendorNumber = row["VendorNumber"].ToString();
+                        oVendorCROSE.Name = row["Name"].ToString();
+                        oVendorCROSE.CheckNumber = row["CheckNumber"].ToString();
+                        oVendorCROSE.CheckDate = DateTime.Parse(row["CheckDate"].ToString());
+                        oVendorCROSE.Amount = double.Parse(row["Amount"].ToString());
+
+                        lstCROSEData.Add(oVendorCROSE);
+                        i++;
+                        oVendorCROSE = null;
+                    }
+
+                }
+            }
+            
+
+            return lstCROSEData;
+        }
+        
         [Route("api/crose/getCroseVendorData/{croseArgs}")]
         [HttpGet]
         public List<VendorCROSE> getCroseVendorData(string croseArgs)
@@ -65,7 +117,7 @@ namespace HuckeWEBAPI.Controllers
 
             //string dbPassword = DecryptString(s_ConnectionStringCROSE, "CroseApp2024");
 
-          
+
             var ConnectionStringPrivate = "server=DWQADB01;uid=app_daep;pwd=";
             //ConnectionStringPrivate += dbPassword;
             ConnectionStringPrivate += ";database=EDB";
@@ -124,7 +176,7 @@ namespace HuckeWEBAPI.Controllers
 
                 }
             }
-            
+
 
             return lstCROSEData;
         }
@@ -287,18 +339,13 @@ namespace HuckeWEBAPI.Controllers
             using (StreamReader r = new StreamReader(path))
             {
                 string json = r.ReadToEnd();
-                _vendorListData = JsonSerializer.Deserialize<List<VendorList>>(json, options);
+                _vendorListData = System.Text.Json.JsonSerializer.Deserialize<List<VendorList>>(json, options);
             }
 
             if (_vendorListData != null && _vendorListData.Count > 0)
             {
                 Console.WriteLine("Number of records: " + _vendorListData.Count);
 
-                /*
-                foreach(var vendor in _vendorListData) {
-                    Console.WriteLine($"{vendor.Name}- {vendor.VendorNumber}");
-                }
-                */
 
                 //using a LINQ QUERY
                 var jnames = _vendorListData.Where(p => p.Name.StartsWith("J"));
@@ -316,5 +363,129 @@ namespace HuckeWEBAPI.Controllers
 
 
         }
+
+        #region FileBasedEndpoints
+        [Route("api/crose/getCroseVendorDataFILE_BASED/{croseArgs}")]
+        [HttpGet]
+        public List<VendorCROSE> getCroseVendorDataFILE_BASED(string croseArgs)
+        {
+
+            var _vendorListData = new List<VendorList>();
+            _vendorListData = GetCroseTransactions();
+
+           string[] _searchValues = croseArgs.Split('|');
+
+
+            VendorCROSE oVendorCROSE;
+            List<VendorCROSE> lstCROSEData = new List<VendorCROSE>();
+
+            switch (_searchValues[4])
+            {
+                case "VendorNumber":
+                    //SQLCommandText += " WHERE VendorNumber LIKE " + "'%" + _searchValues[0] + "%'";
+                    var dataByVendorNumber = _vendorListData.Where(v => v.VendorNumber.Contains(_searchValues[0]));
+                    foreach (var _vendor in dataByVendorNumber)
+                    {
+                        oVendorCROSE = new VendorCROSE();
+                        oVendorCROSE.random = _vendor.random;
+                        oVendorCROSE.VendorNumber = _vendor.VendorNumber;
+                        oVendorCROSE.Name = _vendor.Name;
+                        oVendorCROSE.CheckNumber = _vendor.CheckNumber;
+                        oVendorCROSE.CheckDate = DateTime.Parse(_vendor.CheckDate);
+                        oVendorCROSE.Amount = _vendor.Amount;
+                        lstCROSEData.Add(oVendorCROSE);
+                        oVendorCROSE = null;
+                    }
+                    break;
+                case "VendorName":
+                    //SQLCommandText += " WHERE Name LIKE " + "'%" + _searchValues[1] + "%'";
+                    var dataByVendorName = _vendorListData.Where(v => v.Name.ToLower().Contains(_searchValues[1].ToLower()));
+                    foreach (var _vendor in dataByVendorName)
+                    {
+                        oVendorCROSE = new VendorCROSE();
+                        oVendorCROSE.random = _vendor.random;
+                        oVendorCROSE.VendorNumber = _vendor.VendorNumber;
+                        oVendorCROSE.Name = _vendor.Name;
+                        oVendorCROSE.CheckNumber = _vendor.CheckNumber;
+                        oVendorCROSE.CheckDate = DateTime.Parse(_vendor.CheckDate);
+                        oVendorCROSE.Amount = _vendor.Amount;
+                        lstCROSEData.Add(oVendorCROSE);
+                        oVendorCROSE = null;
+                    }
+                    break;
+                case "VendorNameDate":
+                    //SQLCommandText += " WHERE Name LIKE " + "'%" + _searchValues[1] + "%'" + " AND " + " CheckDate BETWEEN " + "'" + _searchValues[2] + "'" + "AND " + "'" + _searchValues[3] + "'";
+                    var dataByVendorNameAndDateRange = _vendorListData.Where(v => v.Name.ToLower().Contains(_searchValues[1].ToLower()) && DateTime.Parse(v.CheckDate) > Convert.ToDateTime(_searchValues[2]) && DateTime.Parse(v.CheckDate) < Convert.ToDateTime(_searchValues[3]));
+                    foreach (var _vendor in dataByVendorNameAndDateRange)
+                    {
+                        oVendorCROSE = new VendorCROSE();
+                        oVendorCROSE.random = _vendor.random;
+                        oVendorCROSE.VendorNumber = _vendor.VendorNumber;
+                        oVendorCROSE.Name = _vendor.Name;
+                        oVendorCROSE.CheckNumber = _vendor.CheckNumber;
+                        oVendorCROSE.CheckDate = DateTime.Parse(_vendor.CheckDate);
+                        oVendorCROSE.Amount = _vendor.Amount;
+                        lstCROSEData.Add(oVendorCROSE);
+                        oVendorCROSE = null;
+                    }
+
+                    break;
+                case "VendorNumberDate":
+                    //SQLCommandText += " WHERE VendorNumber LIKE " + "'%" + _searchValues[0] + "%'" + " AND " + " CheckDate BETWEEN " + "'" + _searchValues[2] + "'" + "AND " + "'" + _searchValues[3] + "'";
+                    var dataByVVendorNumberDate = _vendorListData.Where(v => v.VendorNumber.Contains(_searchValues[0]) && DateTime.Parse(v.CheckDate) > Convert.ToDateTime(_searchValues[2]) && DateTime.Parse(v.CheckDate) < Convert.ToDateTime(_searchValues[3]));
+                    foreach (var _vendor in dataByVVendorNumberDate)
+                    {
+                        oVendorCROSE = new VendorCROSE();
+                        oVendorCROSE.random = _vendor.random;
+                        oVendorCROSE.VendorNumber = _vendor.VendorNumber;
+                        oVendorCROSE.Name = _vendor.Name;
+                        oVendorCROSE.CheckNumber = _vendor.CheckNumber;
+                        oVendorCROSE.CheckDate = DateTime.Parse(_vendor.CheckDate);
+                        oVendorCROSE.Amount = _vendor.Amount;
+                        lstCROSEData.Add(oVendorCROSE);
+                        oVendorCROSE = null;
+                    }
+
+                    break;
+                default:
+                    break;
+            }
+
+
+            return lstCROSEData;
+
+         
+        }
+        [Route("api/crose/getCroseVendorList_PROD_FILE_BASED/")]
+        [HttpGet]
+        public List<VendorCROSE> getCroseVendorList_PROD_FILE_BASED()
+        {
+
+            var _vendorListData = new List<VendorList>();
+            _vendorListData = GetCroseTransactions();
+
+            var __vendorListDataDistinct = _vendorListData.GroupBy(u => u.Name)
+                                            .Select(grp => grp.ToList())
+                                            .ToList();
+
+            VendorCROSE oVendorCROSE;
+            List<VendorCROSE> lstCROSEData = new List<VendorCROSE>();
+            for (int i = 0; i< __vendorListDataDistinct.Count; i++)
+            {
+                oVendorCROSE = new VendorCROSE();
+                oVendorCROSE.random = __vendorListDataDistinct[i][0].random;
+                oVendorCROSE.VendorNumber = __vendorListDataDistinct[i][0].VendorNumber;
+                oVendorCROSE.Name = __vendorListDataDistinct[i][0].Name;
+                oVendorCROSE.CheckNumber = __vendorListDataDistinct[i][0].CheckNumber;
+                oVendorCROSE.CheckDate = DateTime.Parse(__vendorListDataDistinct[i][0].CheckDate);
+                oVendorCROSE.Amount = __vendorListDataDistinct[i][0].Amount;
+                lstCROSEData.Add(oVendorCROSE);
+                oVendorCROSE = null;
+            }
+
+            return lstCROSEData;
+        }
+
+        #endregion FileBasedEndpoints
     }
 }
